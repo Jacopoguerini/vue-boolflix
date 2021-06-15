05c613a829526abc49ef8391959507e199ce79a0
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-06-15 12:35:52 +0200

    Aggiunto logo in Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,22 @@
 <template>
     <header>
-        <input
-        type="text"
-        placeholder="Titoli"
-        v-model="currentSearchText"
-        @keyup.enter="$emit('performSearch', currentSearchText)">
+        <a href="#">
+            <img src="../assets/img/logo.png" alt="Logo Image">
+        </a>
         
-        <span
-        class="btn search-btn"
-        @click="$emit('performSearch', currentSearchText)">
-        <i class="fas fa-search"></i>
-        Cerca</span>
+        <div class="search-bar">
+            <input
+                type="text"
+                placeholder="Titoli"
+                v-model="currentSearchText"
+                @keyup.enter="$emit('performSearch', currentSearchText)">
+                
+            <span
+            class="btn search-btn"
+            @click="$emit('performSearch', currentSearchText)">
+                <i class="fas fa-search"></i>
+            Cerca</span>
+        </div>
     </header>
 </template>
 
@@ -36,28 +42,42 @@ export default {
 @import '../style/general.scss';
 
     header {
-        height: 80px;
+        height: 70px;
         background-color: grey;
         display: flex;
-        justify-content: flex-end;
+        justify-content: space-between;
         align-items: center;
         padding: 0 100px;
 
-        input {
-            height: 40%;
-            padding-left: 5px;
+        a {
+            height: 100%;
+            display: flex;
+            align-items: center;
 
-            &:focus {
-            outline: none
-            }
-            &::placeholder {
-                color: grey;
+            img {
+                height: 70%;
             }
         }
 
+        .search-bar {
 
-        .fa-search {
-            margin-right: 10px;
+            input {
+                height: 40%;
+                padding-left: 5px;
+                width: 250px;
+
+                &:focus {
+                outline: none
+                }
+                &::placeholder {
+                    color: grey;
+                }
+            }
+
+
+            .fa-search {
+                margin-right: 10px;
+            }
         }
     }
 
